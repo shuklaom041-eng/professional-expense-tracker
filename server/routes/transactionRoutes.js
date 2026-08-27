23f017a4786bb826f68444b7282e95df02f7b967
shuklaom041-eng <shuklaom041@gmail.com>
2026-08-27 23:42:41 +0530

    Fix transaction controller path

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const transactionController =
-    require("../controllers/transactionController");
+    require("../Controllers/transactionController");
 
 const protect =
     require("../middleware/auth");
